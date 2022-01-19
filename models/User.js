1c2b7badc001ca31178e25e65bd7711348f22e4c
dhalladay <dave.halladay@outlook.com>
2022-01-18 18:21:36 -0700

    hashed password functionality

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
@@ -34,6 +35,17 @@ User.init(
     }
   },
   {
+    hooks: {
+      //setup beforeCreate lifecycle hook functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
